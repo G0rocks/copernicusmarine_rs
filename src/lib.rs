5a86edd7fb03e7d276f08691475af982ce774489
G0rocks <huldarh@gmail.com>
2025-06-04 19:34:52 +0000

    First implementation of copernicusmarine subset command. Needs more improvement maybe

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,72 @@
 /// Description
 /// Enables the use of copernicus marine toolbox through rust
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Dependencies
+//-------------------------------------------------------------------------------------------------------------------------
+use time;   // For start and end times
+use std::process::Command; // To run commands through the command line
+// netcdf?
+
+
+// Enums
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+
+// Functions
+//-------------------------------------------------------------------------------------------------------------------------
+/// Function to get a subset from the copernicus database using the copernicus marine toolbox
+pub fn subset(
+    dataset_id: String,
+    variables: Vec<String>,
+    start_datetime: time::UtcDateTime,
+    end_datetime: time::UtcDateTime,
+    minimum_longitude: f64,
+    maximum_longitude: f64,
+    minimum_latitude: f64,
+    maximum_latitude: f64) -> u8 {
+
+    // Make argument
+    let mut args: Vec<String> = vec![
+        "subset".to_string(),
+        "--dataset-id".to_string(),
+        dataset_id];
+
+    // Make variable argument
+    // Loop and for each variable in variables, add "--variable " + variable[i].to_string();
+    for variable in variables {
+        args.push("--variable".to_string());
+        args.push(variable);
+    }
+
+    args.push("--start-datetime".to_string());
+    args.push(start_datetime.to_string());
+    args.push("--end-datetime".to_string());
+    args.push(end_datetime.to_string());
+    args.push("--minimum-longitude".to_string());
+    args.push(minimum_longitude.to_string());
+    args.push("--maximum-longitude".to_string());
+    args.push(maximum_longitude.to_string());
+    args.push("--minimum-latitude".to_string());
+    args.push(minimum_latitude.to_string());
+    args.push("--maximum-latitude".to_string());
+    args.push(maximum_latitude.to_string());
+    
+    let output = Command::new("copernicusmarine")
+        .args(&args)
+        .output()
+        .expect("Failed to execute command \"copernicusmarine subset\" with the given arguments");
+
+    println!("Status: {}", output.status);
+    println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+    return 5;
 }
 
 
 
-// Tests here
+// Tests
+//-------------------------------------------------------------------------------------------------------------------------
 // #[cfg(test)]
 // mod tests {
 //     use super::*;

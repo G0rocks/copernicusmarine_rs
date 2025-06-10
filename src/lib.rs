283f0cd33f748efa517d8e3c5d6fda09825ce66d
G0rocks <huldarh@gmail.com>
2025-06-10 15:41:38 +0000

    Now netcdf works! Still implementing getting actual values though

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@
 
 // Dependencies
 //-------------------------------------------------------------------------------------------------------------------------
-use time;   // For start and end times
+use time;   use core::panic;
+// For start and end times
 use std::process::Command; // To run commands through the command line
 use netcdf; // For working with and using the netcdf files retrieved from the copernicus server
 // use zarrs; // For working with and using the .zarr files retrieved from the copernicus server
@@ -119,14 +120,12 @@ impl Copernicus {
 
         // Make filepath
         let filepath = self.output_path.as_str().to_owned() + "/" + &filename;
-        println!("file path: {}", &filepath);
 
         // set output filename
         args.push("--output-filename".to_string());
-        args.push(filename);
+        args.push(filename.clone());
 
-        println!("Querying server");
-        
+        // println!("Querying server");        
         // Run the command to get the data
         let output = Command::new("copernicusmarine")
             .args(&args)
@@ -135,21 +134,41 @@ impl Copernicus {
 
         // println!("Status: {}", output.status);
         // println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
-        println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+        // println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
 
-        // Find the file where the data is stored
-        // Print hello with pyo3?
-        // pyo3::marker::Python::with_gil(|py| {
-        //     let s = c"print(\"Hello user, I'm Python\")";
-        //     py.run(s, None, None).expect("Well that didn't work");
-        // });
-
-        let netcdf_file = netcdf::open(filename);
-        println!("Netcdf file: {:?}", netcdf_file);
         // Move into output path directory
-        // std::env::set_current_dir(std::path::Path::new(&self.output_path)).expect("Error changing directories");
-        // Print working directory
-        // println!("Changed directory to: {:?}", std::env::current_dir().expect("Could not get current directory"));
+        let start_dir = std::env::current_dir().expect("Could not get current directory");
+        std::env::set_current_dir(std::path::Path::new(&self.output_path)).expect("Error changing directories");
+        // Find the file where the data is stored
+        let nc_filename = filename.as_str().to_owned() + ".nc";
+
+        let netcdf_file = netcdf::open(nc_filename.clone()).expect(format!("Could not get netcdf file: {}", &nc_filename).as_str());
+        let netcdf_root =  netcdf_file.root().expect("Lol nope");
+
+        // Get values from netcdf file
+        let time_stamp = netcdf_root.variable("time").expect("No variable: time");
+        let lat = netcdf_root.variable("latitude").expect("No variable: latitude");
+        let lon = netcdf_root.variable("longitude").expect("No variable: longitude");
+        let east = netcdf_root.variable("eastward_wind").expect("No variable: eastward_wind");
+        // let north = root.variable("northward_wind").expect("No northward_wind var");
+        // println!("east wind: {:?}", east);
+
+        let time_data: Vec<i64> = time_stamp.get_values(netcdf::Extents::All).expect("Failed to read time stamps");
+        let lat_data: Vec<f64> = lat.get_values(netcdf::Extents::All).expect("Failed to read latitude");
+        let lon_data: Vec<f64> = lon.get_values(netcdf::Extents::All).expect("Failed to read latitude");
+        let east_data: Vec<f32> = east.get_values(netcdf::Extents::All).expect("Failed to read eastward wind");
+        // let north_data: Vec<f32> = north.values::<f32>(None, None).expect("Failed to read northward wind");
+        println!("Timestamp: {:?}", secs_since_1990_01_01_0_to_utcdatetime(time_data[0]));
+        println!("Latitude: {:?}", lat_data[0]);
+        println!("Longitude: {:?}", lon_data[0]);
+        println!("Latitude: {:?}", lat_data[1]);
+        println!("Longitude: {:?}", lon_data[1]);
+        println!("Latitude: {:?}", lat_data[2]);
+        println!("Longitude: {:?}", lon_data[2]);
+        println!("east wind: {:?}", east_data[0]);
+        println!("east wind: {:?}", east_data[1]);
+        
+
         // Make zarr store
         // let zarr_store: zarrs::storage::ReadableWritableListableStorage = std::sync::Arc::new(zarrs::storage::store::MemoryStore::new());
         // let zarr_store: zarrs::filesystem::FilesystemStore = zarrs::filesystem::FilesystemStore::new(std::env::current_dir().expect("Could not get current dir")).expect("Could not make zarrs::filesystem::FilesystemStore");
@@ -158,6 +177,10 @@ impl Copernicus {
         // println!("Zarr store keys: {:?}", zarr_store.list());
         // println!("Zarr store : {:?}", zarr_store);
 
+        panic!("Stop run");
+
+        // Move back into starting directory
+        std::env::set_current_dir(start_dir).expect("Error changing directories");
 
         // Return the file
         return 5; //netcdf_file;
@@ -166,6 +189,15 @@ impl Copernicus {
 
 // Helper functions
 //-------------------------------------------------------------------------------------------------------------------------
+/// Makes the seconds since 1990-01-01 00:00:00 into a time::UtcdDateTime value
+/// Since according to datasheet the time is measured in seconds since that date: https://documentation.marine.copernicus.eu/PUM/CMEMS-WIND-PUM-012-004-006.pdf
+pub fn secs_since_1990_01_01_0_to_utcdatetime(secs: i64) -> time::UtcDateTime {
+    let start = time::UtcDateTime::new(time::Date::from_calendar_date(1990,time::Month::January,1).expect("Could not make time::Date"), time::Time::from_hms(0,0,0).expect("Could not make time::Time"));
+
+    let dur = time::Duration::new(secs, 0);
+
+    return start.checked_add(dur).expect("Could not add time::Duration to time::UtcDateTime");
+}
 
 
 /// Writes the datetime variable to the "YYYY-MM-DDTHH:MM:SS" format where the 'T' is literally just a 'T'
